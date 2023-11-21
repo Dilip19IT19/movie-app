139f204bd87e14412d5fad2c8f3e75b98f0ee3ca
Dilip Choudhary <dileepchoudhary469@gmail.com>
2023-11-21 21:26:48 +0530

    localstorage add

--- redux/MovieSlice.tsx
+++ redux/MovieSlice.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { TMovieDetails } from "@/app/movies/[id]/page"
 import { PayloadAction, createSlice } from "@reduxjs/toolkit"
 
@@ -7,18 +8,20 @@ type TMovie={
 }
 
 const initialState:TMovie={
-  movieArr:[]
+  movieArr:JSON.parse(localStorage.getItem("localMovies") || "[]")
 }
 
 const MovieSlice=createSlice({
   name:"favmovies",
   initialState:initialState,
   reducers:{
-    addToFav:(state,action:PayloadAction<TMovieDetails>)=>{
+    addToFav:(state,action:PayloadAction<TMovieDetails>)=>{      
       state.movieArr.push(action.payload);
+      localStorage.setItem("localMovies",JSON.stringify(state.movieArr))
     },
     removeFromFav:(state,action:PayloadAction<TMovieDetails>)=>{
-      state.movieArr=state.movieArr.filter((movie)=>movie.id!==action.payload.id)
+      state.movieArr=state.movieArr.filter((movie)=>movie.id!==action.payload.id);
+      localStorage.setItem("localMovies",JSON.stringify(state.movieArr))
     }
   }
 })
