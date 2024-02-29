5d85bc542bd3af4ea933af44c707ef0731b3d27a
Dilip Choudhary <dileepchoudhary469@gmail.com>
2024-02-29 21:08:00 +0530

    pagination has been added

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -25,13 +25,14 @@ function MovieList() {
 
   let[movies,setMovies]=useState<movie[]>([]);
   let[loading,setLoading]=useState(true);
+  let[page,setPage]=useState(1);
   useEffect(()=>{
   
     async function getMovies()
     {
       try
       {
-        const res=await fetch(`${BASE_URL}/movie/now_playing?language=en-US&page=1&api_key=${API_KEY}`);
+        const res=await fetch(`${BASE_URL}/movie/now_playing?language=en-US&page=${page}&api_key=${API_KEY}`);
         const data=await res.json();
         setMovies(data.results);
       }
@@ -47,11 +48,14 @@ function MovieList() {
       
     }
 
+   
+
     getMovies()
 
-  },[])
+  },[page])
 
   return (
+    <>
     <div className='  list-none mt-4  justify-around flex  flex-wrap gap-8 items-center'>
 
       {loading ? <div className=' w-full h-screen flex justify-center items-center'><span className="loading loading-ring md:loading-lg loading-sm"></span></div> : movies.map((movie)=>{
@@ -60,13 +64,21 @@ function MovieList() {
         )
       }) }
 
-      {/* {movies.map((movie)=>{
-        return(
-          <li key={movie.id}><Card data={movie}/></li>
-        )
-      })} */}
-
     </div>
+
+    <div className=' flex justify-center items-center my-3'>
+      <div className="join">
+        <input className="join-item btn btn-square" type="radio" onChange={(e)=>setPage(parseInt(e.target.value))} name="options" value={1} aria-label="1"  />
+        <input className="join-item btn btn-square" type="radio" onChange={(e)=>setPage(parseInt(e.target.value))} name="options" value={2} aria-label="2" />
+        <input className="join-item btn btn-square" type="radio" onChange={(e)=>setPage(parseInt(e.target.value))} name="options" value={3} aria-label="3" />
+        <input className="join-item btn btn-square" type="radio" onChange={(e)=>setPage(parseInt(e.target.value))} name="options" value={4} aria-label="4" />
+        <input className="join-item btn btn-square" type="radio" onChange={(e)=>setPage(parseInt(e.target.value))} name="options" value={5} aria-label="5" />
+      </div>
+    </div>
+
+    
+
+    </>
   )
 }
 
