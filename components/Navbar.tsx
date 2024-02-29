c8c72b57f9dc8c99fe7991f20363fbcbc3dae64f
Dilip Choudhary <dileepchoudhary469@gmail.com>
2024-02-29 17:29:51 +0530

    added active link feature

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,22 @@
 "use client"
 import { useAppSelector } from '@/hooks';
 import Link from 'next/link'
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
+
+
+const navLinks=[
+  {name:"Home",href:"/"},
+  {name:"Top Rated",href:"/top-rated"},
+  {name:"Popular",href:"/popular"},
+  {name:"Faviourates",href:"/faviourates"},
+]
 
 function Navbar() {
   let[searchText,setSearchText]=useState("");
   let [windowWidth, setWindowWidth] = useState<number>(0);
   const router=useRouter();
+  const pathname=usePathname();
 
   useEffect(() => {
     function handleResize() {
@@ -65,6 +74,8 @@ function Navbar() {
         <li><Link className='hover:text-[#8ebbff]  text-sm text-slate-500  ' href="/top-rated">Top Rated</Link></li>
         <li><Link className='hover:text-[#8ebbff]  text-sm text-slate-500  ' href="/popular">Popular</Link></li>
         <li><Link className='hover:text-[#8ebbff]  text-sm text-slate-500 ' href="/faviourates">Faviourates</Link></li>
+
+       
       </ul>
     </div>
   </div>
@@ -79,20 +90,25 @@ function Navbar() {
 
 <div className=' flex gap-12  '>
 
-  <Link className='hover:text-[#8ebbff]  text-lg  text-slate-500 mx-2 ' href="/">Home</Link>
+  {/* <Link className='hover:text-[#8ebbff]  text-lg  text-slate-500 mx-2 ' href="/">Home</Link>
   <Link className='hover:text-[#8ebbff]  text-lg text-slate-500 mx-2 ' href="/top-rated">Top Rated</Link>
   <Link className='hover:text-[#8ebbff]  text-lg text-slate-500 mx-2 ' href="/popular">Popular</Link>
-  <Link className='hover:text-[#8ebbff]  text-lg text-slate-500 mx-2 ' href="/faviourates">Faviourates</Link>
+  <Link className='hover:text-[#8ebbff]  text-lg text-slate-500 mx-2 ' href="/faviourates">Faviourates</Link> */}
+
+  {navLinks.map((movie)=>{
+    let isActive= pathname===movie.href;
+    return (
+      <Link href={movie.href} className={ isActive ? "font-bold text-xl  text-[#8ebbff]" : "hover:text-[#8ebbff]  text-lg text-slate-500 mx-2"}>{movie.name}</Link>
+    )
+
+  })}
+  
+
 
 </div>
 
 <div className='flex gap-10'>
-  {/* <label tabIndex={0} className="btn btn-ghost btn-circle">
-  <div className="indicator">
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-    <span className="badge badge-sm indicator-item">{useAppSelector((state)=>state.favMovies.movieArr.length)}</span>
-  </div>
-</label> */}
+ 
   <input type="text" placeholder='search any movie' value={searchText} onKeyDown={(e)=>handleCheck(e)} onChange={(e)=>setSearchText(e.target.value)} className=' w-48 md:w-72 p-1 opacity-90 rounded-md border-none text-center' />
   <button className=' mr-6 ml-2  md:mr-4 md:ml-2 p-1 rounded-md text-[#8ebbff] border-[1px] border-[#8ebbff] hover:border-none hover:bg-[#8ebbff] hover:text-black active:scale-90 transition-scale duration-300' onClick={handleSubmit}>Search</button>
 </div>
