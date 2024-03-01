ae186b917ff2bc3a6c9d895f1035ceade484b87d
Dilip Choudhary <dileepchoudhary469@gmail.com>
2024-03-01 21:58:00 +0530

    Added  sorry message in each route

---
+++ components/Info.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Info() {
+  return (
+    <div className=' w-full h-screen grid place-content-center'> 
+      <p className=' md:w-[400px] md:text-lg w-[280px] text-sm'>Sorry everyone 🙏. I have created this project using an API from the <a target='_blank' className=' hover:underline text-blue-400' href="https://www.themoviedb.org/">The Movie Database (TMDB)</a> which is currently not working in India. <span className='text-orange-300 '>Kindly use vpn</span> . Trust me you will not regeret after seeing my project. </p>
+    </div>
+  )
+}
+
+export default Info
